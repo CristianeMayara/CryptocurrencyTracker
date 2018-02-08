e551fd83821026754fd21db8eb13fa261b2b270e
Cristiane Mayara <cris.mayara@gmail.com>
2018-02-08 12:29:37 -0300

    Add the provider

--- app/App.js
+++ app/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
+import Store from "./Store";
 import Header from "./components/Header";
 
 export default class App extends Component {
   render() {
     return (
-      <View>
-        <Header />
-      </View>
+      <Provider store={Store}>
+        <View>
+          <Header />
+        </View>
+      </Provider>
     );
   }
 }
